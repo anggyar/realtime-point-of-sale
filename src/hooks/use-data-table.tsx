57d0f2facae0f454549c7f259f70c556dee8bf85
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-08-29 02:00:20 +0700

    Add search functionality to useDataTable hook; implement debounced search state management

--- src/hooks/use-data-table.tsx
+++ src/hooks/use-data-table.tsx
@@ -1,9 +1,14 @@
 import { DEFAULT_LIMIT, DEFAULT_PAGE } from "@/constants/data-table-constan";
 import React, { useState } from "react";
+import useDebounce from "./use-debounce";
 
 export default function useDataTable() {
   const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
   const [currentLimit, setCurrentLimit] = useState(DEFAULT_LIMIT);
+  const [currentSearch, setCurrentSearch] = useState("");
+
+  const debounce = useDebounce();
+
   const handleChangePage = (page: number) => {
     setCurrentPage(page);
   };
@@ -12,10 +17,19 @@ export default function useDataTable() {
     setCurrentLimit(limit);
     setCurrentPage(DEFAULT_PAGE);
   };
+
+  const handleChangeSearch = (search: string) => {
+    debounce(() => {
+      setCurrentSearch(search);
+      setCurrentPage(DEFAULT_PAGE);
+    }, 500);
+  };
   return {
     currentPage,
     handleChangePage,
     currentLimit,
     handleChangeLimit,
+    currentSearch,
+    handleChangeSearch,
   };
 }
