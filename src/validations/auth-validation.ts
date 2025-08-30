6b0f70a6a61138a6b62cd37b0acae3565b7f0ecf
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-08-31 03:23:16 +0700

    Add createUserSchema for user registration validation

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -5,5 +5,14 @@ export const loginSchemaForm = z.object({
   password: z.string().min(1, "Password is required"),
 });
 
+export const createUserSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Please enter a valid email"),
+  password: z.string().min(1, "Password is required"),
+  name: z.string().min(1, "Name is required"),
+  role: z.string().min(1, "Role is required"),
+  // avatar_url: z.union([z.string().min(1, "image URL is required"), z.instanceof(File)]),
+});
+
 // Lebih menghemat menggunakan infer daripada harus menulis ulang type nya.
 export type LoginForm = z.infer<typeof loginSchemaForm>;
+export type createUserForm = z.infer<typeof createUserSchema>;
