3eab8aa9422892829ce618bd8098717b95384d2a
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-01 01:26:51 +0700

    Enhance user creation dialog: add avatar upload and role selection components

--- src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -12,14 +12,17 @@ import { Form } from "@/components/ui/form";
 import {
   INITIAL_CREATE_USER_FORM,
   INITIAL_STATE_CREATE_USER,
+  ROLE_LIST,
 } from "@/constants/auth-constant";
 import { createUserForm, createUserSchema } from "@/validations/auth-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
-import { startTransition, useActionState, useEffect } from "react";
+import { startTransition, useActionState, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { createUser } from "../actions";
+import FormSelect from "@/components/common/form-select";
+import FormImage from "@/components/common/form-image";
 
 export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const form = useForm<createUserForm>({
@@ -32,11 +35,14 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
     INITIAL_STATE_CREATE_USER
   );
 
+  const [preview, setPreview] = useState<{ file: File; displayUrl: string } | undefined>(
+    undefined
+  );
   // FUNGSI SUBMIT
   const onSubmit = form.handleSubmit(async (data) => {
     const formData = new FormData();
     Object.entries(data).forEach(([key, value]) => {
-      formData.append(key, value);
+      formData.append(key, key === "avatar_url" ? preview!.file ?? "" : value);
     });
 
     startTransition(() => {
@@ -54,6 +60,7 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
     if (createUserState?.status === "success") {
       toast.success("User Created Successfully");
       form.reset();
+      setPreview(undefined);
       document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
       refetch();
     }
@@ -86,11 +93,19 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
             type='email'
           />
 
-          <FormInput
+          <FormImage
+            form={form}
+            name='avatar_url'
+            label='Avatar'
+            preview={preview}
+            setPreview={setPreview}
+          />
+
+          <FormSelect
             form={form}
             name='role'
             label='Role'
-            placeholder='Insert your role'
+            selectItem={ROLE_LIST}
           />
 
           <FormInput
