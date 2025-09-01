cd00c8ad7ccb5b3252446ff4c80ae0f90576dadb
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-01 14:52:07 +0700

    improve type usage on preview props

--- src/components/common/form-image.tsx
+++ src/components/common/form-image.tsx
@@ -5,6 +5,7 @@ import { Input } from "../ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { FileImage } from "lucide-react";
 import { getImageData } from "@/lib/utils";
+import { Preview } from "@/types/general";
 
 export default function FormImage<T extends FieldValues>({
   form,
@@ -16,11 +17,8 @@ export default function FormImage<T extends FieldValues>({
   form: UseFormReturn<T>;
   name: Path<T>;
   label: string;
-  preview?: {
-    file: File;
-    displayUrl: string;
-  };
-  setPreview?: (preview: { file: File; displayUrl: string }) => void;
+  preview?: Preview;
+  setPreview?: (preview: Preview) => void;
 }) {
   return (
     <FormField
