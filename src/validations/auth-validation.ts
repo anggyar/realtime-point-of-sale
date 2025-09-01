bf6d18d2ef64a341457db8d996e29c2758c8e58c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-01 16:30:03 +0700

    create schema for updating user and add constant for initial state before updating user

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -13,6 +13,13 @@ export const createUserSchema = z.object({
   avatar_url: z.union([z.string().min(1, "image URL is required"), z.instanceof(File)]),
 });
 
+export const updateUserSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  role: z.string().min(1, "Role is required"),
+  avatar_url: z.union([z.string().min(1, "image URL is required"), z.instanceof(File)]),
+});
+
 // Lebih menghemat menggunakan infer daripada harus menulis ulang type nya.
 export type LoginForm = z.infer<typeof loginSchemaForm>;
 export type createUserForm = z.infer<typeof createUserSchema>;
+export type updateUserForm = z.infer<typeof updateUserSchema>;
