0976c7a049d7cd7b8cf1ab80d89dd4511f515ab4
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-03 02:28:03 +0700

    change the directory for user management

---
+++ src/app/(dashboard)/admin/user/user.tsx
@@ -0,0 +1,158 @@
+"use client";
+import DataTable from "@/components/common/data-table";
+import DropdwondAction from "@/components/common/drowpdown-action";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { HEADER_TABLE_USER } from "@/constants/user-constant";
+import useDataTable from "@/hooks/use-data-table";
+import { createClient } from "@/lib/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { Pencil, Trash2 } from "lucide-react";
+import { useMemo, useState } from "react";
+import { toast } from "sonner";
+import DialogCreateUser from "./_components/dialog-create-user";
+import { Profile } from "@/types/auth";
+import DialogUpdateUser from "./_components/dialog-update-user";
+import DialogDeleteUser from "./_components/dialog-delete-user";
+
+export default function UserManagement() {
+  const supabase = createClient();
+
+  // Paggil hook useDataTable
+  const {
+    currentLimit,
+    currentPage,
+    currentSearch,
+    handleChangeLimit,
+    handleChangePage,
+    handleChangeSearch,
+  } = useDataTable();
+
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users", currentPage, currentLimit, currentSearch],
+    queryFn: async () => {
+      const result = await supabase
+        .from("profiles")
+        .select("*", { count: "exact" })
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order("created_at")
+        .ilike("name", `%${currentSearch}%`);
+
+      if (result.error)
+        toast.error("Get User Data failed", {
+          description: result.error.message,
+        });
+
+      console.info(result);
+      console.info(currentSearch);
+      return result;
+    },
+  });
+
+  const [selectedActions, setSelectedActions] = useState<{
+    type: "update" | "delete";
+    data: Profile;
+  } | null>(null);
+
+  const handleChangeAction = (open: boolean) => {
+    if (!open) {
+      setSelectedActions(null);
+    }
+  };
+
+  const filteredData = useMemo(() => {
+    return (users?.data || []).map((user, index) => {
+      return [
+        currentLimit * (currentPage - 1) + index + 1,
+        user.id,
+        user.name,
+        user.role,
+        <DropdwondAction
+          menu={[
+            {
+              label: (
+                <span className='flex items-center gap-2'>
+                  <Pencil />
+                  Edit
+                </span>
+              ),
+              action: () => {
+                setSelectedActions({
+                  data: user,
+                  type: "update",
+                });
+              },
+            },
+            {
+              label: (
+                <span className='flex items-center gap-2'>
+                  <Trash2 className='text-red-400' />
+                  Delete
+                </span>
+              ),
+              variant: "destructive",
+              action: () => {
+                setSelectedActions({
+                  data: user,
+                  type: "delete",
+                });
+              },
+            },
+          ]}
+        />,
+      ];
+    });
+  }, [users]);
+
+  const totalPages = useMemo(() => {
+    return users && users.count !== null ? Math.ceil(users.count / currentLimit) : 0;
+  }, [users]);
+
+  return (
+    <div className='w-full'>
+      <div className='flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full'>
+        <h1 className='text-2xl font-bold'>User Management</h1>
+        <div className='flex gap-2'>
+          <Input
+            placeholder='Search by name'
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant='outline'>Create</Button>
+            </DialogTrigger>
+            <DialogCreateUser refetch={refetch} />
+          </Dialog>
+        </div>
+      </div>
+      <DataTable
+        header={HEADER_TABLE_USER}
+        isLoading={isLoading}
+        data={filteredData}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
+      />
+      <DialogUpdateUser
+        open={selectedActions !== null && selectedActions.type === "update"}
+        refetch={refetch}
+        currentData={selectedActions?.data}
+        handleChangeAction={handleChangeAction}
+      />
+
+      <DialogDeleteUser
+        open={selectedActions !== null && selectedActions.type === "delete"}
+        refetch={refetch}
+        currentData={selectedActions?.data}
+        handleChangeAction={handleChangeAction}
+      />
+    </div>
+  );
+}
