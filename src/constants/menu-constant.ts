f95b00359b8b9f9310691e9f4faa5baddc99590c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-04 02:26:27 +0700

    create CATEGORY_LIST

--- src/constants/menu-constant.ts
+++ src/constants/menu-constant.ts
@@ -6,3 +6,61 @@ export const HEADER_TABLE_MENU = [
   "Available",
   "Action",
 ];
+
+export const CATEGORY_LIST = [
+  {
+    value: "makanan_utama",
+    label: "Makanan Utama",
+  },
+  {
+    value: "cemilan",
+    label: "Cemilan",
+  },
+  {
+    value: "minuman_kopi",
+    label: "Minuman Kopi",
+  },
+  {
+    value: "minuman_non_kopi",
+    label: "Minuman Non-Kopi",
+  },
+  {
+    value: "dessert",
+    label: "Dessert",
+  },
+];
+
+export const AVAILABILITY_LIST = [
+  {
+    value: "true",
+    label: "Available",
+  },
+  {
+    value: "false",
+    label: "Not Available",
+  },
+];
+
+export const INITIAL_CREATE_MENU_FORM = {
+  name: "",
+  description: "",
+  price: "",
+  discount: "",
+  category: "",
+  image_url: "",
+  is_available: "",
+};
+
+export const INITIAL_STATE_CREATE_MENU = {
+  status: "idle",
+  errors: {
+    name: [],
+    description: [],
+    price: [],
+    discount: [],
+    category: [],
+    image_url: [],
+    is_available: [],
+    _form: [],
+  },
+};
