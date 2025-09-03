7282d2a151b17dd95abcde9e5ba08c795b4504f4
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-04 02:26:54 +0700

    create menu schema

--- src/validations/menu-validation.ts
+++ src/validations/menu-validation.ts
@@ -9,5 +9,15 @@ export const menuSchema = z.object({
   image_url: z.union([z.string(), z.instanceof(File)]),
   is_available: z.boolean(),
 });
+export const createMenuSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  price: z.string().min(1, "Price is required"),
+  discount: z.string().min(1, "Discount is required"),
+  category: z.string().min(1, "Category is required"),
+  image_url: z.union([z.string().min(1, "Image URL is required"), z.instanceof(File)]),
+  is_available: z.string().min(1, "Availability is required"),
+});
 
+export type CreateMenuForm = z.infer<typeof createMenuSchema>;
 export type Menu = z.infer<typeof menuSchema> & { id: string };
