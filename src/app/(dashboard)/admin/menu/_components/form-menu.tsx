6a6cc3f1940b35692774eb4bd0698a79e9882e2c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-04 02:25:15 +0700

    feat: create Form Menu

---
+++ src/app/(dashboard)/admin/menu/_components/form-menu.tsx
@@ -0,0 +1,117 @@
+import FormImage from "@/components/common/form-image";
+import FormSelect from "@/components/common/form-select";
+import FormInput from "@/components/common/input-form";
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+
+import { AVAILABILITY_LIST, CATEGORY_LIST } from "@/constants/menu-constant";
+import { Preview } from "@/types/general";
+import { Loader2 } from "lucide-react";
+import { FormEvent } from "react";
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+
+export default function FormMenu<T extends FieldValues>({
+  form,
+  onSubmit,
+  isLoading,
+  type,
+  preview,
+  setPreview,
+}: {
+  form: UseFormReturn<T>;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  isLoading: boolean;
+  type: "Create" | "Update";
+  preview?: Preview;
+  setPreview?: (preview: Preview) => void;
+}) {
+  return (
+    <DialogContent className='sm:max-w-[425px] max-h-[90vh]'>
+      <DialogHeader>
+        <DialogTitle className='items-center font-bold text-2xl'>{type} Menu</DialogTitle>
+        <DialogDescription className='font-semibold'>
+          {type === "Create" ? "Add a new menu" : "Make changes menu here"}
+        </DialogDescription>
+      </DialogHeader>
+      <Form {...form}>
+        <form
+          className='space-y-4'
+          onSubmit={onSubmit}
+        >
+          <div className='space-y-4 max-h-[50vh] py-1 overflow-y-auto'>
+            <FormInput
+              form={form}
+              name={"name" as Path<T>}
+              label='Name'
+              placeholder='Insert menu name'
+            />
+            <FormInput
+              form={form}
+              name={"description" as Path<T>}
+              label='Description'
+              placeholder='Insert menu description'
+              type='textarea'
+            />
+
+            <FormSelect
+              form={form}
+              name={"category" as Path<T>}
+              label='Category'
+              selectItem={CATEGORY_LIST}
+            />
+
+            <FormInput
+              form={form}
+              name={"price" as Path<T>}
+              label='Price'
+              placeholder='Insert price here'
+              type='number'
+            />
+            <FormInput
+              form={form}
+              name={"discount" as Path<T>}
+              label='Discount'
+              placeholder='Insert discount here'
+              type='number'
+            />
+
+            <FormImage
+              form={form}
+              name={"image_url" as Path<T>}
+              label='Image'
+              preview={preview}
+              setPreview={setPreview}
+            />
+
+            <FormSelect
+              form={form}
+              name={"is_available" as Path<T>}
+              label='Availability'
+              selectItem={AVAILABILITY_LIST}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant={"outline"}>Cancel</Button>
+              </DialogClose>
+              <Button
+                type='submit'
+                className=''
+              >
+                {isLoading ? <Loader2 className='animate-spin' /> : type}
+              </Button>
+            </DialogFooter>
+          </div>
+        </form>
+      </Form>
+    </DialogContent>
+  );
+}
