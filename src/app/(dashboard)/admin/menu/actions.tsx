2fe406d671c530c18b0c4bc3e25aa83bddaa0d56
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-04 12:09:29 +0700

    add update menu function

--- src/app/(dashboard)/admin/menu/actions.tsx
+++ src/app/(dashboard)/admin/menu/actions.tsx
@@ -78,3 +78,82 @@ export async function createMenu(prevState: MenuFormState, formData: FormData) {
     status: "success",
   };
 }
+
+export async function updateMenu(prevState: MenuFormState, formData: FormData) {
+  let validateFields = menuSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+    price: parseFloat(formData.get("price") as string),
+    discount: parseFloat(formData.get("discount") as string),
+    category: formData.get("category"),
+    image_url: formData.get("image_url"),
+    is_available: formData.get("is_available") === "true" ? true : false,
+  });
+
+  if (!validateFields.success) {
+    return {
+      status: "error",
+      errors: {
+        ...validateFields.error.flatten().fieldErrors,
+        _form: [],
+      },
+    };
+  }
+
+  if (validateFields.data.image_url instanceof File) {
+    const oldImageUrl = formData.get("old_image_url") as string;
+    const { errors, data } = await uploadFile(
+      "images",
+      "menus",
+      validateFields.data.image_url,
+      oldImageUrl.split("/images/")[1]
+    );
+
+    if (errors) {
+      return {
+        status: "error",
+        errors: {
+          ...prevState.errors,
+          _form: [...errors._form],
+        },
+      };
+    }
+
+    validateFields = {
+      ...validateFields,
+      data: {
+        ...validateFields.data,
+        image_url: data.url,
+      },
+    };
+  }
+
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("menus")
+    .update({
+      name: validateFields.data.name,
+      description: validateFields.data.description,
+      price: validateFields.data.price,
+      discount: validateFields.data.discount,
+      category: validateFields.data.category,
+      image_url: validateFields.data.image_url,
+      is_available: validateFields.data.is_available,
+    })
+    .eq("id", formData.get("id"));
+
+  if (error) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [error.message],
+      },
+    };
+  }
+
+  return {
+    status: "success",
+  };
+}
