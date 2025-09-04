bcdb94e4a750701dda63469a54a29711aba41ee5
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-04 12:11:44 +0700

    add updateMenuSchema for update the menu

--- src/validations/menu-validation.ts
+++ src/validations/menu-validation.ts
@@ -19,5 +19,16 @@ export const createMenuSchema = z.object({
   is_available: z.string().min(1, "Availability is required"),
 });
 
+export const updateMenuSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  price: z.string().min(1, "Price is required"),
+  discount: z.string().min(1, "Discount is required"),
+  category: z.string().min(1, "Category is required"),
+  image_url: z.union([z.string().min(1, "Image URL is required"), z.instanceof(File)]),
+  is_available: z.string().min(1, "Availability is required"),
+});
+
 export type CreateMenuForm = z.infer<typeof createMenuSchema>;
 export type Menu = z.infer<typeof menuSchema> & { id: string };
+export type UpdateMenuForm = z.infer<typeof updateMenuSchema>;
