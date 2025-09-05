903a428a1951552929f7608a2f658d6429015287
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-05 21:54:30 +0700

    add Table management page

---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TableManagement from "./_components/table";
+
+export const metadata = {
+  title: "Jadi Ria | Table Management",
+};
+export default function TableManagementPage() {
+  return <TableManagement />;
+}
