fa3a678cdf5b911a512d82451bfd283f7c485a4a
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-05 21:58:19 +0700

    Add Table Constant  for initial create and state table

---
+++ src/constants/table-constant.ts
@@ -0,0 +1,34 @@
+export const HEADER_TABLE_TABLES = ["No", "Name", "Capacity", "Status", "Action"];
+
+export const INITIAL_CREATE_TABLE_FORM = {
+  name: "",
+  description: "",
+  capacity: "",
+  status: "",
+};
+
+export const INITIAL_STATE_CREATE_TABLE = {
+  status: "idle",
+  errors: {
+    name: [],
+    description: [],
+    capacity: [],
+    status: [],
+    _form: [],
+  },
+};
+
+export const STATUS_TABLE_LIST = [
+  {
+    value: "available",
+    label: "Available",
+  },
+  {
+    value: "unavailable",
+    label: "Unavailable",
+  },
+  {
+    value: "reserved",
+    label: "Reserved",
+  },
+];
