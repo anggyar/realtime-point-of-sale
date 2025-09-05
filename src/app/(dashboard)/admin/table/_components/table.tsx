844ec88939b6775b43e28a7950cc91c0d663639d
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-05 21:56:51 +0700

    add Table Management template

---
+++ src/app/(dashboard)/admin/table/_components/table.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import DataTable from "@/components/common/data-table";
+import DropdownAction from "@/components/common/drowpdown-action";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import useDataTable from "@/hooks/use-data-table";
+import { createClient } from "@/lib/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { Pencil, Trash2 } from "lucide-react";
+import { useMemo, useState } from "react";
+import { toast } from "sonner";
+import { cn } from "@/lib/utils";
+import { Table } from "@/validations/table-validation";
+import { HEADER_TABLE_TABLES } from "@/constants/table-constant";
+import DialogCreateTable from "./dialog-create-table";
+
+export default function TableManagement() {
+  const supabase = createClient();
+
+  const {
+    currentPage,
+    currentLimit,
+    currentSearch,
+    handleChangePage,
+    handleChangeLimit,
+    handleChangeSearch,
+  } = useDataTable();
+
+  const {
+    data: tables,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["tables", currentPage, currentLimit, currentSearch],
+    queryFn: async () => {
+      const query = supabase
+        .from("tables")
+        .select("*", { count: "exact" })
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order("created_at");
+
+      if (currentSearch) {
+        query.or(
+          `name.ilike.%${currentSearch}%,capacity.ilike.%${currentSearch}%,capacity.ilike.%${currentSearch}%`
+        );
+      }
+
+      const result = await query;
+
+      if (result.error)
+        toast.error("Get Table data failed", {
+          description: result.error.message,
+        });
+
+      return result;
+    },
+  });
+
+  const [selectedAction, setSelectedAction] = useState<{
+    data: Table;
+    type: "update" | "delete";
+  } | null>(null);
+
+  const handleChangeAction = (open: boolean) => {
+    if (!open) setSelectedAction(null);
+  };
+
+  const filteredData = useMemo(() => {
+    return (tables?.data || []).map((table: Table, index) => {
+      return [
+        currentLimit * (currentPage - 1) + index + 1,
+        <div>
+          <h4 className='font-bold mb-2'>{table.name}</h4>
+          <p className='text-xs'>{table.description}</p>
+        </div>,
+        table.capacity,
+        <div
+          className={cn("px-2 py-1 rounded-full text-white w-fit capitalize", {
+            "bg-green-600": table.status === "available",
+            "bg-red-600": table.status === "unavailable",
+            "bg-yellow-600": table.status === "reserved",
+          })}
+        >
+          {table.status}
+        </div>,
+        // <div
+        //   className={cn(
+        //     "px-2 py-1 rounded-full text-white w-fit",
+        //     tabl.is_available ? "bg-green-600" : "bg-red-500"
+        //   )}
+        // >
+        //   {menu.is_available ? "Available" : "Not Available"}
+        // </div>,
+        <DropdownAction
+          menu={[
+            {
+              label: (
+                <span className='flex item-center gap-2'>
+                  <Pencil />
+                  Edit
+                </span>
+              ),
+              action: () => {
+                setSelectedAction({
+                  data: table,
+                  type: "update",
+                });
+              },
+            },
+            {
+              label: (
+                <span className='flex item-center gap-2'>
+                  <Trash2 className='text-red-400' />
+                  Delete
+                </span>
+              ),
+              variant: "destructive",
+              action: () => {
+                setSelectedAction({
+                  data: table,
+                  type: "delete",
+                });
+              },
+            },
+          ]}
+        />,
+      ];
+    });
+  }, [tables]);
+
+  const totalPages = useMemo(() => {
+    return tables && tables.count !== null ? Math.ceil(tables.count / currentLimit) : 0;
+  }, [tables]);
+
+  return (
+    <div className='w-full'>
+      <div className='flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full'>
+        <h1 className='text-2xl font-bold'>Table Management</h1>
+        <div className='flex gap-2'>
+          <Input
+            placeholder='Search by name or category'
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant='outline'>Create</Button>
+            </DialogTrigger>
+            {/* <DialogCreateMenu refetch={refetch} /> */}
+            <DialogCreateTable refetch={refetch} />
+          </Dialog>
+        </div>
+      </div>
+      <DataTable
+        header={HEADER_TABLE_TABLES}
+        data={filteredData}
+        isLoading={isLoading}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
+      />
+    </div>
+  );
+}
