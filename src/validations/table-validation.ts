f5f459b2fdae117b15cee6a80ba575d3a50dd3a4
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-05 23:21:57 +0700

    add update table schema and add type updateTableForm

--- src/validations/table-validation.ts
+++ src/validations/table-validation.ts
@@ -14,5 +14,13 @@ export const createTableSchema = z.object({
   status: z.string().min(1, "Status is required"),
 });
 
+export const updateTableSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(10, "Description is required"),
+  capacity: z.string().min(1, "Capacity is required"),
+  status: z.string().min(1, "Status is required"),
+});
+
 export type Table = z.infer<typeof tableSchema> & { id: string };
 export type createTableForm = z.infer<typeof createTableSchema>;
+export type updateTableForm = z.infer<typeof updateTableSchema>;
