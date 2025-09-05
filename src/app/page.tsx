feae1743fcfe46f84163c702e341a3876c4c4bb7
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-05 21:49:11 +0700

    fixing typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
-      <h1 className="text-4xl font-semibold space-y-4">Welcome Anggyar Muhamad Yahya</h1>
+    <div className='bg-muted flex justify-center items-center h-screen flex-col space-y-4'>
+      <h1 className='text-4xl font-semibold space-y-4'>Welcome Anggyar Muhamad Yahya</h1>
       <Link href={"/admin"}>
-        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+        <Button className='bg-teal-500 text-white'>Access Dashboard</Button>
       </Link>
     </div>
   );
