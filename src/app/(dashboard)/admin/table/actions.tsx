4f51c0891285d00bdc83f13e1904ea6271e58d6c
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-06 02:22:47 +0700

    update deleteTable function for delete Table reservation

--- src/app/(dashboard)/admin/table/actions.tsx
+++ src/app/(dashboard)/admin/table/actions.tsx
@@ -8,8 +8,7 @@ import { menuSchema } from "@/validations/menu-validation";
 import { tableSchema } from "@/validations/table-validation";
 
 export async function createTable(prevState: TableFormState, formData: FormData) {
-  // eslint-disable-next-line prefer-const
-  let validateFields = tableSchema.safeParse({
+  const validateFields = tableSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
     capacity: parseInt(formData.get("capacity") as string),
@@ -54,7 +53,7 @@ export async function updateTable(prevState: TableFormState, formData: FormData)
   const validateFields = tableSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
-    capacity: parseFloat(formData.get("capacity") as string),
+    capacity: parseInt(formData.get("capacity") as string),
     status: formData.get("status"),
   });
 
@@ -95,23 +94,10 @@ export async function updateTable(prevState: TableFormState, formData: FormData)
   };
 }
 
-export async function deleteMenu(prevState: MenuFormState, formData: FormData) {
+export async function deleteTable(prevState: TableFormState, formData: FormData) {
   const supabase = await createClient();
-  const image = formData.get("image_url") as string;
 
-  const { status, errors } = await deleteFile("images", image.split("/images/")[1]);
-
-  if (status === "error") {
-    return {
-      status: "error",
-      errors: {
-        ...prevState.errors,
-        _form: [errors?._form?.[0] ?? "Unknown error"],
-      },
-    };
-  }
-
-  const { error } = await supabase.from("menus").delete().eq("id", formData.get("id"));
+  const { error } = await supabase.from("tables").delete().eq("id", formData.get("id"));
 
   if (error) {
     return {
