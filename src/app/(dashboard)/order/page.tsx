fe1daf421be3b4fa82af9fd95d3f7b7caba04d51
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-07 15:26:25 +0700

    call OrderManagement component on page order

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "Jadi Ria | Order Management",
+};
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
