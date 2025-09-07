73e94857fd0a5d42cf2f0b19771c3f721ba1e111
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-08 01:23:59 +0700

    create function DetailOrder

---
+++ src/app/(dashboard)/order/[id]/_components/detail-order.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import React, { useMemo } from "react";
+
+import DataTable from "@/components/common/data-table";
+import Link from "next/link";
+import { HEADER_TABLE_DETAIL_ORDER } from "@/constants/order-constant";
+import { createClient } from "@/lib/supabase/client";
+import useDataTable from "@/hooks/use-data-table";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import Image from "next/image";
+import { cn, convertIDR } from "@/lib/utils";
+
+export default function DetailOrder({ id }: { id: string }) {
+  const supabase = createClient();
+
+  const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
+    useDataTable();
+
+  /* ------------------------- QUERY DATA FROM TABLES AND ORDERS------------------------- */
+  const { data: order } = useQuery({
+    queryKey: ["order", id],
+    queryFn: async () => {
+      const result = await supabase
+        .from("orders")
+        .select(
+          `
+            id, customer_name, status, payment_url, tables (name, id)
+          `
+        )
+        .eq("order_id", id)
+        .single();
+
+      if (result.error)
+        toast.error("Get Order data failed", {
+          description: result.error.message,
+        });
+
+      return result.data;
+    },
+    enabled: !!id,
+  });
+
+  const { data: orderMenu, isLoading: isLoadingOrdermenu } = useQuery({
+    queryKey: ["orders_menu", order?.id, currentPage, currentLimit],
+    queryFn: async () => {
+      const result = await supabase
+        .from("orders_menu")
+        .select("*, menus (id, name, image_url, price)", { count: "exact" })
+        .eq("order_id", order?.id)
+        .order("status");
+
+      if (result.error)
+        toast.error("Get Order menu data failed", {
+          description: result.error.message,
+        });
+
+      return result;
+    },
+    enabled: !!order?.id,
+  });
+
+  const filteredData = useMemo(() => {
+    return (orderMenu?.data || []).map((item, index) => {
+      return [
+        currentLimit * (currentPage - 1) + index + 1,
+        <div className='flex items-center gap-2'>
+          <Image
+            src={item.menus.image_url}
+            alt={item.menus.name}
+            width={40}
+            height={40}
+            className='rounded'
+          />
+        </div>,
+        <div className='flex flex-col'>
+          {item.menus.name} x {item.quantitiy}
+          <span className='text-xs text-muted-foreground'>
+            {item.notes || "No notes"}
+          </span>
+        </div>,
+        <div>{convertIDR(item.menus.price * item.quantity)}</div>,
+        <div
+          className={cn("px-2 py-1 rounded-full text-white w-fit capitalize", {
+            "bg-gray-500": item.status === "pending",
+            "bg-yellow-500": item.status === "process",
+            "bg-blue-500": item.status === "ready",
+            "bg-green-500": item.status === "serve",
+          })}
+        >
+          {item.status}
+        </div>,
+      ];
+    });
+  }, [orderMenu?.data]);
+
+  const totalPages = useMemo(() => {
+    return orderMenu && orderMenu.count !== null
+      ? Math.ceil(orderMenu.count / currentLimit)
+      : 0;
+  }, [orderMenu]);
+  return (
+    <div className='w-full space-y-4'>
+      <div className='flex items-center justify-between gap-4 w-full'>
+        <h1 className='text-2xl font-bold'>Detail Order</h1>
+        <Link href={""}>
+          <Button>Add Order Item</Button>
+        </Link>
+      </div>
+
+      <div className='flex flex-col lg:flex-row gap-4 w-full'>
+        <div className='lg:w-2/3'>
+          <DataTable
+            header={HEADER_TABLE_DETAIL_ORDER}
+            data={filteredData}
+            isLoading={isLoadingOrdermenu}
+            totalPages={totalPages}
+            currentPage={currentPage}
+            currentLimit={currentLimit}
+            onChangePage={handleChangePage}
+            onChangeLimit={handleChangeLimit}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
