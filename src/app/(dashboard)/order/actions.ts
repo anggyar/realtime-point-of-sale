35bd3eedc15de6fa19c97fb75c5f8f4c0d9d6200
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-07 20:33:52 +0700

    add updateReservation function, user now can change status on order page, then update it too on tables status

--- src/app/(dashboard)/order/actions.ts
+++ src/app/(dashboard)/order/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
+import { FormState } from "@/types/general";
 
 import { OrderFormState } from "@/types/order";
 import { TableFormState } from "@/types/table";
@@ -42,6 +43,46 @@ export async function createOrder(prevState: OrderFormState, formData: FormData)
         status: validateFields.data.status === "reserved" ? "reserved" : "unavailable",
       })
       .eq("id", validateFields.data.table_id),
+  ]);
+
+  const orderError = orderResult.error;
+  const tableError = tableResult.error;
+
+  if (orderError || tableError) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [
+          ...(orderError ? [orderError.message] : []),
+          ...(tableError ? [tableError.message] : []),
+        ],
+      },
+    };
+  }
+
+  return {
+    status: "success",
+  };
+}
+
+export async function updateReservation(prevState: FormState, formData: FormData) {
+  const supabase = await createClient();
+
+  const [orderResult, tableResult] = await Promise.all([
+    supabase
+      .from("orders")
+      .update({
+        status: formData.get("status"),
+      })
+      .eq("id", formData.get("id")),
+
+    supabase
+      .from("tables")
+      .update({
+        status: formData.get("status") === "process" ? "unavailable" : "available",
+      })
+      .eq("id", formData.get("table_id")),
   ]);
 
   const orderError = orderResult.error;
