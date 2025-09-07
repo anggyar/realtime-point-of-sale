bd8a8d923a02563c5332133057f6ddeff8dd4b21
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-07 15:29:58 +0700

    customize app sidebar to give good reability and contrast ratio when hovering icon

--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -35,15 +35,18 @@ export default function AppSidebar() {
   const pathname = usePathname();
   const profile = useAuthStore((state) => state.profile);
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar collapsible='icon'>
       {/* HEADER SIDEBAR */}
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size={"lg"} asChild>
-              <div className="font-semibold">
-                <div className="bg-teal-600 font-medium p-2 justify-center rounded-md">
-                  <Soup className="size-4" />
+            <SidebarMenuButton
+              size={"lg"}
+              asChild
+            >
+              <div className='font-semibold'>
+                <div className='bg-primary font-medium p-2 justify-center rounded-md'>
+                  <Soup className='size-4' />
                 </div>
                 Jadi Ria
               </div>
@@ -54,15 +57,18 @@ export default function AppSidebar() {
 
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupContent className="flex flex-col gap-2">
+          <SidebarGroupContent className='flex flex-col gap-2'>
             <SidebarMenu>
               {SIDEBAR_MENU_LIST[profile.role as SidebarMenuKey]?.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild tooltip={item.title}>
+                  <SidebarMenuButton
+                    asChild
+                    tooltip={item.title}
+                  >
                     <a
                       href={item.url}
                       className={cn("px-4 py3 h-auto", {
-                        "bg-teal-500 text-white hover:bg-teal-500 hover:text-white":
+                        "bg-sidebar-primary text-white hover:bg-sidebar-primary-foreground hover:text-white":
                           pathname === item.url,
                       })}
                     >
@@ -85,41 +91,47 @@ export default function AppSidebar() {
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton
                   size={"lg"}
-                  className="data[state=open]:bg-sidebar-accent data[state=open]:text-sidebar-accent-foreground"
+                  className='data[state=open]:bg-sidebar-accent data[state=open]:text-sidebar-accent-foreground'
                 >
-                  <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={profile.avatar_url} alt={profile.name} />
-                    <AvatarFallback className="rounded-lg">
+                  <Avatar className='h-8 w-8 rounded-lg'>
+                    <AvatarImage
+                      src={profile.avatar_url}
+                      alt={profile.name}
+                    />
+                    <AvatarFallback className='rounded-lg'>
                       {profile.name?.charAt(0)}
                     </AvatarFallback>
                   </Avatar>
-                  <div className="leading-tight">
-                    <h4 className="truncate font-medium">{profile.name}</h4>
-                    <p className="text-muted-foreground truncate text-xs capitalize">
+                  <div className='leading-tight'>
+                    <h4 className='truncate font-medium'>{profile.name}</h4>
+                    <p className='text-muted-foreground truncate text-xs capitalize'>
                       {profile.role}
                     </p>
                   </div>
 
-                  <EllipsisVertical className="ml-auto size-4" />
+                  <EllipsisVertical className='ml-auto size-4' />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
               <DropdownMenuContent
-                className="min-w-56 rounded-lg"
+                className='min-w-56 rounded-lg'
                 side={isMobile ? "bottom" : "right"}
-                align="end"
+                align='end'
                 sideOffset={4}
               >
-                <DropdownMenuLabel className="p-0 font-normal">
-                  <div className="flex items-center gap-2 px-1 py-1.5">
-                    <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarImage src={profile.avatar_url} alt={profile.name} />
-                      <AvatarFallback className="rounded-lg">
+                <DropdownMenuLabel className='p-0 font-normal'>
+                  <div className='flex items-center gap-2 px-1 py-1.5'>
+                    <Avatar className='h-8 w-8 rounded-lg'>
+                      <AvatarImage
+                        src={profile.avatar_url}
+                        alt={profile.name}
+                      />
+                      <AvatarFallback className='rounded-lg'>
                         {profile.name?.charAt(0)}
                       </AvatarFallback>
                     </Avatar>
-                    <div className="leading-tight">
-                      <h4 className="truncate font-medium">{profile.name}</h4>
-                      <p className="text-muted-foreground truncate text-xs capitalize">
+                    <div className='leading-tight'>
+                      <h4 className='truncate font-medium'>{profile.name}</h4>
+                      <p className='text-muted-foreground truncate text-xs capitalize'>
                         {profile.role}
                       </p>
                     </div>
