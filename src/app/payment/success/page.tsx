9d34d7da95c07f37cccb9a49fe0b2e195790c274
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-17 01:44:08 +0700

    Create succes page for success payment

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Success from "./_components/success";
+
+export const metadata = {
+  title: "Jadi Ria | Payment Success",
+};
+
+export default function SuccessPage() {
+  return <Success />;
+}
