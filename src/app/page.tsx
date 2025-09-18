b5fd3284a00a8e4aee971cca16a2ce2c88931cdc
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-18 15:21:35 +0700

    add Authentication data user to update Welcome Message, now it can recognize user name and redirect page based on user role

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/stores/auth-store";
 
 import Link from "next/link";
 
 export default function Home() {
+  const profile = useAuthStore((state) => state.profile);
+  console.log(profile.role);
   return (
     <div className='bg-muted flex justify-center items-center h-screen flex-col space-y-4'>
-      <h1 className='text-4xl font-semibold space-y-4'>Welcome Anggyar Muhamad Yahya</h1>
-      <Link href={"/admin"}>
-        <Button className='bg-teal-500 text-white'>Access Dashboard</Button>
+      <h1 className='text-4xl font-semibold space-y-4'>
+        Welcome {profile.name}
+      </h1>
+      <Link href={profile.role === "admin" ? "/admin" : "/order"}>
+        <Button className='bg-teal-500 hover:bg-teal-700 active:bg-teal-900 text-white'>
+          Access Dashboard
+        </Button>
       </Link>
     </div>
   );
